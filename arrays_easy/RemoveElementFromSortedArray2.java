348377878c4be3505e4bdd78c6f58e031c1f47e1
Borui Wang <wangbd@bc.edu>
2016-12-08 13:51:44 -0600

    added comments

--- arrays_easy/RemoveElementFromSortedArray2.java
+++ arrays_easy/RemoveElementFromSortedArray2.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Source:https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+similar analysis with RemoveElementFromSortedArray
  */
 package Leetcode_Java.arrays_easy;
 
