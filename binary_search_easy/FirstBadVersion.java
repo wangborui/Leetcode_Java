c49a1523fcf3e51190b75f369c3dd5805ba58f72
Borui Wang <wangbd@bc.edu>
2016-12-14 20:17:19 -0600

    Analysis of first bad version

--- binary_search_easy/FirstBadVersion.java
+++ binary_search_easy/FirstBadVersion.java
@@ -1,5 +1,27 @@
 /*
- * regardless n is even or odd, s = good, e = bad
+source: https://leetcode.com/problems/first-bad-version/
+
+Conditions: this question assumes there is always one bad version in the batch
+Inputs:
+1. first one is bad B,B,B,B,B,B.....
+2. second one is bad G,B,B,B,B,B....
+3. bad in mid G,G,G,G,B,B,B...
+4. last bad G,G,G,G,G,G,B...
+5. just one entry B
+
+*Solutions:
+If we set left pointer to 1, right to n, and find mid = left + (right - left) / 2
+*Invariant: 
+left is always pointing to good version
+right is always pointing to the first bad version
+*Loop ending conditions:
+while(left + 1 < right)
+left and right pointers are the same when there is only one entry, terminate loop
+find middle of left and right 
+left is always next to right pointer when loop ends
+
+
+ * 
  */
 package Leetcode_Java.binary_search_easy;
 
@@ -8,29 +30,14 @@ package Leetcode_Java.binary_search_easy;
  * @author Borui Wang
  */
 public class FirstBadVersion {
-//    public int firstBadVersion(int  n) {
-//        int s = 1, e = n, res = e;
-//        while(s + 1 < e) {
-//            int mid = (s + e) >>> 1;
-//            if(isBadVersion(mid)) {
-//                e = mid;
-//            }
-//            else {
-//                s = mid;
-//            }
-//        }
-//        if(isBadVersion(e)) {
-//            res = e;
-//        }
-//        if(isBadVersion(s)) {
-//            res = s;
-//        }
-//        return res;
-//    }
-    
     static int firstBadVersion(int [] n) {
-        int s = 0, e = n.length - 1, res = e;
+        if(n[0] == 8) return 0;
+        int s = 0, e = n.length - 1;
+        //left is good
+        //right is bad
         while(s + 1 < e) {
+           //left is good
+           //right is bad
             int mid = (s + e) >>> 1;
             //8 is bad, 2 is good
             if(n[mid] == 8) {
@@ -40,17 +47,14 @@ public class FirstBadVersion {
                 s = mid;
             }
         }
-        if(n[e] == 8) {
-            res = e;
-        }
-        if(n[s] == 8) {
-            res = s;
-        }
-        return res;
+        //left is good
+        //right is first bad
+        return e;
     }
     public static void main(String[] args){
          // int [][] m = new int[][]{{-1},{-1}};
          
-        System.out.println(firstBadVersion(new int[]{2,2,2,8,8,8,8}));
+        //System.out.println(firstBadVersion(new int[]{2,2,2,8,8,8,8}));
+        System.out.println(firstBadVersion(new int[]{2,8,8,8,8}));
     }
 }
