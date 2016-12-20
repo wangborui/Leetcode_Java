8aa0b288c099c4b1e32d502d6b824b9ad7644575
Borui Wang <wangbd@bc.edu>
2016-12-20 10:58:35 -0600

    Inorder traversal three ways

--- tree_medium/InorderTraversal.java
+++ tree_medium/InorderTraversal.java
@@ -15,7 +15,6 @@ import java.util.Stack;
  */
 public class InorderTraversal {
     static class TreeNode {
-
         int val;
         TreeNode left;
         TreeNode right;
@@ -39,20 +38,64 @@ public class InorderTraversal {
         helper(list, root.right);
         //add right
     }
-    public ArrayList<Integer> inorderTraversalIterative(TreeNode root) {
+    static ArrayList<Integer> inorderTraversalIterative(TreeNode root) {
         Stack<TreeNode> stack = new Stack<TreeNode>();
         ArrayList<Integer> result = new ArrayList<Integer>();
         TreeNode curt = root;
         while (curt != null || !stack.empty()) {
+            //keep going down left sub-tree until null
+            //this means left node is null
             while (curt != null) {
                 stack.add(curt);
                 curt = curt.left;
             }
-            curt = stack.peek();
-            stack.pop();
+            //now go to "mid" node
+            //get last inserted left sub-tree node
+            curt = stack.pop();
             result.add(curt.val);
+            //check "right" node
             curt = curt.right;
         }
         return result;
+    }
+    public List<Integer> inorderTraversalDivideNConquer(TreeNode root) {
+         ArrayList<Integer> result = new ArrayList<Integer>();
+        // null or leaf
+        if (root == null) {
+            return result;
+        }
+
+        // Divide
+        List<Integer> left = inorderTraversalDivideNConquer(root.left);
+        List<Integer> right = inorderTraversalDivideNConquer(root.right);
+
+        // Conquer
+        result.addAll(left);
+        result.add(root.val);
+        result.addAll(right);
+        return result;
+    }
+     public static void main(String[] args) {
+        /*create tree
+         *      1
+         *     / \
+         *    3   7
+         *  /   /  \
+         * 6   8    10
+         *
+        */
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(3);
+        TreeNode c = new TreeNode(7);
+        TreeNode d = new TreeNode(6);
+        TreeNode e = new TreeNode(8);
+        TreeNode f = new TreeNode(10);
+        
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        c.left = e;
+        c.right = f;
+        System.out.println(inorderTraversalIterative(a));
     }
 }
