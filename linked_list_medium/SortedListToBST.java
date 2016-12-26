fd8a207e3d0cfd73f6b8ec253c526d87852999b6
Borui Wang <wangbd@bc.edu>
2016-12-25 22:35:04 -0600

    Solution 2 Time O(n)

--- linked_list_medium/SortedListToBST.java
+++ linked_list_medium/SortedListToBST.java
@@ -72,11 +72,48 @@ public class SortedListToBST {
         root.left = sortedListToBST(head);
         return root;
     }
+   //Time O(1), saves time from finding mid node 
+    private static ListNode current;
+    static TreeNode sortedListToBSTWOMID(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        
+         current = head;
+         int length = 0;
+         for(ListNode p = head; p != null; p = p.next) {
+             length++;
+         }
+         return inOrderTraversal(0, length - 1);
+    }
+    //[3,5,8]
+    static TreeNode inOrderTraversal(int start, int end) {
+        if(current == null || start > end) {
+            return null;
+        }
+        
+        int mid = start + (end - start) / 2;
+        TreeNode left = inOrderTraversal(start, mid - 1);
+        TreeNode root = new TreeNode(current.val);
+        current = current.next;
+        TreeNode right = inOrderTraversal(mid + 1, end);
+        
+        root.left = left;
+        root.right = right;
+        
+        return root;
+        
+    }
     
     public static void main(String[] args){
-        ListNode a = new ListNode(1);
-        ListNode b = new ListNode(2);
+        ListNode a = new ListNode(3);
+        ListNode b = new ListNode(5);
+        ListNode c = new ListNode(8);
         a.next = b;
-        sortedListToBST(a);
+        b.next = c;
+        //TreeNode tree = sortedListToBST(a);
+        
+       TreeNode tree = sortedListToBSTWOMID(a);
+        System.out.println();
     }
 }
