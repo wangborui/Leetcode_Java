10f91c5f3bb895ad685200b11917d15fa288d94f
Borui Wang <wangbd@bc.edu>
2016-12-27 13:21:18 -0600

    unique paths with obstacles

--- dynamic_programming_medium/UniquePathWithObstacles2.java
+++ dynamic_programming_medium/UniquePathWithObstacles2.java
@@ -1,46 +1,74 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Source: https://leetcode.com/problems/unique-paths-ii/
+********************************************************************************
+Follow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+
+Note: m and n will be at most 100.
+********************************************************************************
  */
-package dynamic_programming_medium;
+package Leetcode_Java.dynamic_programming_medium;
 
 /**
  *
  * @author Borui Wang
  */
 public class UniquePathWithObstacles2 {
-       static int uniquePathsWithObstacles(int[][] obstacleGrid) {
-         
-        if(obstacleGrid.length == 0 || obstacleGrid[0].length == 0) return 1;
+//As same as DP solution with "Unique Path I", just need to consider the obstacles.
+
+    static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+//obstacle = 1, empty space = 0
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        
-        int [] moves = new int [n];
-        
-        for(int i = 0; i < m;i++){
-            for(int j = 0; j < n;j++){
-                //first row
-                if(i == 0){
-                    if(obstacleGrid[i][j] == 1)
-                        break;
-                    moves[j] =1;
-                     
-                }
-                //first column
-                else if(j == 0){
-                    moves[j] = (obstacleGrid[i][j] == 1)?0:1;
-                }
-                else{
-                    moves[j] = (obstacleGrid[i][j] == 1)?0:moves[j] + moves[j-1];
+
+        int[][] dp = new int[m][n];
+
+        //set first row to 1
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+
+        //set first col to 1
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+        // dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
         }
-        return moves[n-1];
-    
-     
+
+        return dp[m - 1][n - 1];
+
     }
-       public static void main(String[] args){
-           System.out.println(uniquePathsWithObstacles(new int[][]{{1},{0}}));
-       }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePathsWithObstacles(new int[][]{{1}, {0}}));
+    }
 }
