b71be03f007f546002d2b42ccb62b81083ed7c61
Borui Wang <wangbd@bc.edu>
2017-01-02 10:38:09 -0600

    combination sum without duplicates

--- arrays_medium/CombinationSum.java
+++ arrays_medium/CombinationSum.java
@@ -1,12 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Source: https://leetcode.com/problems/combination-sum/
+Date: 01/02/2017
+********************************************************************************
+Given a set of candidate numbers (C) (without duplicates) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [2, 3, 6, 7] and target 7, 
+A solution set is: 
+[
+  [7],
+  [2, 2, 3]
+]
+********************************************************************************
  */
 package Leetcode_Java.arrays_medium;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,14 +30,12 @@ public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> lists = new ArrayList();
         List<Integer> list = new ArrayList();
-        //there may be duplicate values in candidates, but our answer must not have duplicates
-        Arrays.sort(candidates);
         helper(lists, list, candidates, target, 0);
         return lists;
     }
     private void helper(List<List<Integer>> lists, List<Integer> list, int[] candidates, int remain, int s){
         if(remain == 0) {
-            //new instance of list, because it changes 
+            //new instance of list, because it changes NOT lists.add(list)
             lists.add(new ArrayList(list));
             return;
         }
@@ -33,10 +44,6 @@ public class CombinationSum {
             return;
         }
         for(int i = s; i < candidates.length; i++){
-            //remove duplicates from the solution 
-            if(i != s && candidates[i] == candidates[i-1]) {
-                continue;
-            }
             list.add(candidates[i]);
             //use i instead of i + 1, because the same index can be used many times
             helper(lists, list, candidates, remain - candidates[i], i);
