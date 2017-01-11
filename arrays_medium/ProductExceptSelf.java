bc4554ae013f9688d8cd7156b279361b05ff9884
Borui Wang <wangbd@bc.edu>
2017-01-11 15:30:13 -0600

    product of itself right and left

---
+++ arrays_medium/ProductExceptSelf.java
@@ -0,0 +1,52 @@
+/*
+Source : https://leetcode.com/problems/product-of-array-except-self/
+Date   : 01/11/2017
+********************************************************************************
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+********************************************************************************
+
+ */
+package Leetcode_Java.arrays_medium;
+
+/**
+ *
+ * @author Borui Wang
+ */
+//Given numbers [2, 3, 4, 5], regarding the third number 4, the product of array except 4 is 2*3*5 
+//which consists of two parts: left 2*3 and right 5. The product is left*right. We can get lefts and rights:
+//
+//Numbers:     2    3    4     5
+//Lefts:            2   2*3  2*3*4
+//Rights:   3*4*5  4*5   5      
+//Let’s fill the empty with 1:
+//
+//Numbers:     2    3    4     5
+//Lefts:       1    2   2*3  2*3*4
+//Rights:   3*4*5  4*5   5     1
+//We can calculate lefts and rights in 2 loops. The time complexity is O(n).
+//
+//We store lefts in result array. If we allocate a new array for rights. The space complexity is O(n). To make it O(1), 
+public class ProductExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int [] res = new int[nums.length];
+        //first store all left products except i in res[i]
+        int rightP = 1;
+        //calculate all left products
+        for(int i = 0; i < nums.length;i++){
+            res[i] = (i==0)?1: res[i-1]*nums[i-1];
+        }
+        //find the right products
+        for(int i = nums.length -1; i >= 0; i--){
+            rightP = (i==nums.length-1)?1:rightP * nums[i+1];
+            res[i] = res[i]*rightP;
+        }
+        return res;
+    }
+}
