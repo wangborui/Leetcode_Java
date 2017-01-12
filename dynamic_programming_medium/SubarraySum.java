4b10ade6cf90691370c4c6ea3df14f7f09cf9f53
Borui Wang <wangbd@bc.edu>
2017-01-12 09:19:02 -0600

    subarray sum with prefix sum

---
+++ dynamic_programming_medium/SubarraySum.java
@@ -0,0 +1,50 @@
+/*
+Source : http://www.lintcode.com/en/problem/subarray-sum/
+Date   : 01/12/2017
+********************************************************************************
+Given an integer array, find a subarray where the sum of numbers is zero. 
+Your code should return the index of the first number and the index of the last number.
+********************************************************************************
+
+ */
+package Leetcode_Java.dynamic_programming_medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Borui Wang
+ */
+public class SubarraySum {
+        /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     * Find the prefix sum of an array
+     *        if prefix sum[i] == sum[j] for i < j, 
+     *      we know that sum[i + 1...j] must be 0
+     *      therefore, the start index of array is i + 1, and end index j
+     */
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap();
+        ArrayList<Integer> res = new ArrayList();
+        
+        //addresses for test case [1,-1], and set index -1 to be 0
+        //also where sum starts at index -1 with value 0
+        map.put(0, -1);
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(map.containsKey(sum)) {
+               res.add(map.get(sum) + 1);
+               res.add(i);
+               return res;
+            } else {
+               map.put(sum, i); 
+            }
+        }
+        return res;
+    }
+}
