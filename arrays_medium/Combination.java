9d1c81ca44f1f584ab52afc7cbe08c97fa6a34c0
Borui Wang <wangbd@bc.edu>
2017-01-16 12:22:18 -0600

    Combinations of numbers n

--- arrays_medium/Combination.java
+++ arrays_medium/Combination.java
@@ -1,8 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+// Source : https://oj.leetcode.com/problems/combinations/
+// Date   : 01/16/2017
+
+/********************************************************************************** 
+* 
+* Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+* 
+* For example,
+* If n = 4 and k = 2, a solution is:
+* 
+* [
+*   [2,4],
+*   [3,4],
+*   [2,3],
+*   [1,2],
+*   [1,3],
+*   [1,4],
+* ]
+* 
+*               
+**********************************************************************************/
 package Leetcode_Java.arrays_medium;
 
 import java.util.ArrayList;
@@ -19,14 +36,14 @@ public class Combination {
         combineHelper(lists, list, n, k, 1);
         return lists;
     }
-    private void combineHelper(List<List<Integer>> lists, List<Integer> list, int n, int k, int s){
+    private void combineHelper(List<List<Integer>> lists, List<Integer> list, int n, int k, int start){
         if(list.size() == k) {
             //instantiate a new instance of arraylist
             lists.add(new ArrayList(list));
             return;
         }
         
-        for(int i = s; i <= n; i++){
+        for(int i = start; i <= n; i++){
             list.add(i);
             combineHelper(lists, list, n, k, i+1);
             list.remove(list.size() - 1);
