6d2ae9b12a82f6068ac26d477dc16c38b5ef8424
Borui Wang <wangbd@bc.edu>
2017-01-17 15:19:54 -0600

    Simplified code

--- binary_search_medium/Search2DMatrix.java
+++ binary_search_medium/Search2DMatrix.java
@@ -15,22 +15,19 @@ public class Search2DMatrix {
             return false;
         }
         //View the entire matrix as one row from 0 to m*n -1, m is row n is col. 
-        int m = matrix.length, n = matrix[0].length, s = 0, e = m * n - 1;
+        int m = matrix.length, n = matrix[0].length, start = 0, end = m * n - 1;
         //Do a binary search on that one whole matrix
-        while(s + 1 < e){
-            int mid = (s + e) >>> 1;
+        while(start + 1 < end){
+            int mid = (start + end) >>> 1;
             //when we find a mid, we need to correspond the value of mid to some val in matrix, matrix[mid/n][mid%n] is that value
-            if(matrix[mid / n][mid % n] == target) {
-                e = mid;
-            }
-            else if(matrix[mid / n][mid % n] > target) {
-                e = mid;
+            if(matrix[mid / n][mid % n] >= target) {
+                end = mid;
             }
             else {
-                s = mid;
+                start = mid;
             }
         }
-        if(matrix[s / n][s % n] == target || matrix[e / n][e % n] == target) {
+        if(matrix[start / n][start % n] == target || matrix[end / n][end % n] == target) {
             return true;
         }
         return false;
