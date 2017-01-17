c3a0959b9e04b92d1fbfd932b1c18f4ff9e92572
Borui Wang <wangbd@bc.edu>
2017-01-17 15:59:38 -0600

    Added Comments

--- binary_search_medium/FindPeakElement.java
+++ binary_search_medium/FindPeakElement.java
@@ -1,13 +1,36 @@
 /*
+
+// Source : https://oj.leetcode.com/problems/find-peak-element/
+// Date   : 01/17/2017
+
+/********************************************************************************** 
+* 
+* A peak element is an element that is greater than its neighbors.
+* 
+* Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+* 
+* You may imagine that num[-1] = num[n] = -∞.
+* 
+* For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+* 
+* click to show spoilers.
+* 
+* Note:
+* Your solution should be in logarithmic complexity.
+* 
+* Credits:Special thanks to @ts for adding this problem and creating all test cases.
+*               
+********************************************************************************* 
+
 2 similar questions:
 case 1:
  this question condition -infinity{0- - - - -numbers}-infinity
-so we set s = 0, e = n - 1, and when mid go out of bound we set it to -inifity 
+so we set start = 0, end = n - 1, and when mid go out of bound we set it to -inifity 
 
 case 2: 
 The numbers in adjacent positions are different.
 A[0] < A[1] && A[A.length - 2] > A[A.length - 1].
-we need to set s  = 1, e = n - 2 to avoid overflow
+we need to set start  = 1, end = n - 2 to avoid overflow
 time O(log n) space O(1)
 
 Another clever time O(n)
@@ -28,24 +51,44 @@ package Leetcode_Java.binary_search_medium;
  * @author Borui Wang
  */
 public class FindPeakElement {
+    /*
+ *    Binary search is common idea here.
+ *
+ *    However, you need to think about two senarios:
+ *
+ *    1) Becasue we need check `num[mid-1]`, `num[mid]`, `num[mid+1]`, 
+ *       So, we need make sure there hasn't out-of-boundary issue.
+ *   
+ *
+ *
+ *    2) There are multiple Peak elements.
+ *
+ *       For example: [1,2,1,2,1], or [ 1,2,3,1,2,1]
+ *
+ *       LeetCode doesn't tell you what the expected result is. I guess:
+ *
+ *       2.1) for [1,2,1,2,1] you can return either 1 or 3, because both them are peak elements
+ *
+ *       2.1) for [1,2,3,2,4,2,1] it should return 4, because num[4] is the real peak.  but Leetcode accept either 2 or 4
+ *
+ */
     static int findPeakElement(int[] nums) {
-        int s = 0, e = nums.length - 1;
-        while(s + 1 < e){
-            int mid = (s + e) >>> 1;
-            //made a mistake  <= 0, should be  >= 0
+        int start = 0, end = nums.length - 1;
+        while(start + 1 < end){
+            int mid = (start + end) >>> 1;
             int prev = (mid - 1) >= 0 ? nums[mid-1] : Integer.MIN_VALUE;
             if(nums[mid] < prev) {
-                e = mid;
+                end = mid;
             }
             else {
-                s = mid;
+                start = mid;
             }
         }
-        if(nums[s] < nums[e]) {
-            return e;
+        if(nums[start] < nums[end]) {
+            return end;
         }
         else {
-            return s;
+            return start;
         }
     }
     public static void main(String [] args){
