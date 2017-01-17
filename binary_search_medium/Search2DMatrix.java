7b6fd357bb466fc612f5a1f880e5e441ab2e71b1
Borui Wang <wangbd@bc.edu>
2017-01-17 15:34:49 -0600

    Added Comments

--- binary_search_medium/Search2DMatrix.java
+++ binary_search_medium/Search2DMatrix.java
@@ -1,8 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Source : https://oj.leetcode.com/problems/search-a-2d-matrix/
+// Date   : 01/17/2017
+
+/********************************************************************************** 
+* 
+* Write an efficient algorithm that searches for a value in an m x n matrix. 
+* This matrix has the following properties:
+* 
+* Integers in each row are sorted from left to right.
+* The first integer of each row is greater than the last integer of the previous row.
+* 
+* For example,
+* 
+* Consider the following matrix:
+* 
+* [
+*   [1,   3,  5,  7],
+*   [10, 11, 16, 20],
+*   [23, 30, 34, 50]
+* ]
+* 
+* Given target = 3, return true.
+*               
+**********************************************************************************/
 package Leetcode_Java.binary_search_medium;
 
 /**
