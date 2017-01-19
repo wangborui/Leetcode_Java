b59fd24b9ab3df65570c42c5320ea7908c8adbc4
Borui Wang <wangbd@bc.edu>
2017-01-19 14:40:32 -0600

    Directory update

--- tree_medium/LowestCommonAncestors.java
+++ tree_medium/LowestCommonAncestors.java
@@ -3,7 +3,8 @@ Source: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 ********************************************************************************
 Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
 
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
+According to the definition of LCA on Wikipedia: 
+“The lowest common ancestor is defined between two nodes v and w as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
 
         _______3______
        /              \
