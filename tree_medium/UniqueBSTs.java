5f15cbbb3052d459b35350f30f2d3d241f937459
Borui Wang <wangbd@bc.edu>
2017-01-19 20:16:30 -0600

    Update comments

--- tree_medium/UniqueBSTs.java
+++ tree_medium/UniqueBSTs.java
@@ -36,6 +36,11 @@ Let's iterate all the possibilities of this question
     42 ways  n = 5   1,2,3,4,5
                                 T[4] + T[1] * T[3] + T[2] * T[2] + T[3] * T[1] + T[4] = 14 + 5 + 4 + 5 + 14 = 42 ways
 
+So, we first know that 
+when n = 0 there is one way for the BST, which is null
+when n = 1 there is one way for the BST, which is just n
+when n = i, we need to iterate all values of n, and find total ways we can make its left subtree and right subtree, and multiply the possibilities of the two
+
 So here is the optimal function:
     Intialization: T[0] = 1 and T[1] = 1
         T[i] = Sum(T[0 : i - 1] * T[i - 1 : 0])
@@ -52,7 +57,7 @@ public class UniqueBSTs {
         //initialization
         dp[0] = 1;
         dp[1] = 1;
-        
+        //        T[i] = Sum(T[0 : i - 1] * T[i - 1 : 0])
         for(int i = 2; i <= n; i++) {
             for(int j = 0; j < i; j++) {
                 dp[i] += dp[j] * dp[i - 1 - j];
