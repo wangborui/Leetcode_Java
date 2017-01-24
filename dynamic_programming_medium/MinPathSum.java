5a3a1986253ae4cacdcdd1015b2fa894a7d7fba8
Borui Wang <wangbd@bc.edu>
2017-01-24 12:41:24 -0600

    Added space optimization method

--- dynamic_programming_medium/MinPathSum.java
+++ dynamic_programming_medium/MinPathSum.java
@@ -18,32 +18,58 @@ package Leetcode_Java.dynamic_programming_medium;
  * @author Borui Wang
  */
 public class MinPathSum {
+
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        
+
         //sum[i][j] means the min path sum from grid[0][0] to i,j
-        int [][] sum = new int[m][n];
-        
+        int[][] sum = new int[m][n];
+
         //initializing starting point
         sum[0][0] = grid[0][0];
-        
+
         //initializing first row
-        for(int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             sum[0][i] = grid[0][i] + sum[0][i - 1];
         }
-        
+
         //initializing first col
-        for(int i = 1; i < m; i++) {
+        for (int i = 1; i < m; i++) {
             sum[i][0] = grid[i][0] + sum[i - 1][0];
         }
-        
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
                 sum[i][j] = Math.min(sum[i - 1][j], sum[i][j - 1]) + grid[i][j];
             }
         }
-        
+
         return sum[m - 1][n - 1];
+    }
+
+    public int minPathSumSpaceOptimized(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] sum = new int[n];
+        sum[0] = grid[0][0];
+        
+        //initializationn
+        for(int i = 1; i < n; i++) {
+            sum[i] = sum[i - 1] + grid[0][i]; 
+        }
+        //sum[i][j] means the min path sum from grid[0][0] to i,j
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(j == 0) {
+                    sum[j] = sum[j] + grid[i][j];
+                } else {
+                    sum[j] = Math.min(sum[j], sum[j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return sum[n - 1];
+
     }
 }
