3776a8f2a447926d743b5ad3c981f92492eb9553
Borui Wang <wangbd@bc.edu>
2017-01-30 12:40:14 -0600

    Find max tree of array using stack

---
+++ stack_hard/MaxTree.java
@@ -0,0 +1,93 @@
+/*
+Source : https://github.com/shawnfan/LintCode/blob/master/Java/Max%20Tree.java 
+Date   : 01/30/2017
+
+********************************************************************************
+Given an integer array with no duplicates. A max tree building on this array is defined as follow:
+The root is the maximum number in the array
+The left subtree and right subtree are the max trees of the subarray divided by the root number.
+Construct the max tree by the given array.
+Example
+Given [2, 5, 6, 0, 3, 1], the max tree constructed by this array is:
+    6
+   / \
+  5   3
+ /   / \
+2   0   1
+Challenge
+O(n) time and memory.
+********************************************************************************
+
+Solutions:
+Recursion: Time O(n^2) worst case O(n log n) average case
+Iteration: Time O(n) Space O(n)
+ */
+package Leetcode_Java.stack_hard;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Borui Wang
+ */
+public class MaxTree {
+
+    static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    /**
+     * 
+     * @param A
+     * @return TreeNode
+     * Analysis:
+     * 
+     * Iterate the array from left to right. Assume that every element we take is the current right most element
+     * Goal: find the left child of this element, and find parent of this element
+     * We push every element into stack during iteration
+     * 
+     *      1.) left-child: using while loop to find the largest element in the stack that is smaller than current element, and set that element as left child of current element
+     *      2.) if there are still elements left in stack, that element must be the parent of current element, current element must be right child of that element 
+     *      3.) after iterating all element, the last element in stack must be the root of all elements, therefore, we pop all elements in stack to get that last element
+     * 
+     * Note: Interesting behavior:
+     * Stack: always stores the largest value at bottom. In above example, when 6 gets in stack, it will never come back.
+     * All smaller element (smaller than current point) will be popped out, 
+     * and of course, the last-possible-smaller element will be the largest smaller point on stack, then we attach it to current node.
+     * These behavior keeps larger value on upper level of the tree
+     */
+    static TreeNode maxTree(int[] A) {
+        if(A == null || A.length == 0) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack();
+        for(int i = 0; i < A.length; i++) {
+            TreeNode node = new TreeNode(A[i]);
+            //1.) left-child: using while loop to find the largest element in the stack that is smaller than current element, and set that element as left child of current element
+            while(!stack.isEmpty() && node.val >= stack.peek().val) {
+                node.left = stack.pop();
+            }
+            // 2.) if there are still elements left in stack, that element must be the parent of current element, current element must be right child of that element 
+            if(!stack.isEmpty()) {
+                stack.peek().right = node;
+            }
+            stack.push(node);
+        }
+        
+        TreeNode res = stack.peek();
+        //3.) after iterating all element, the last element in stack must be the root of all elements, therefore, we pop all elements in stack to get that last element
+        while(!stack.isEmpty()) {
+            res = stack.pop();
+        }
+        return res;
+    }
+    public static void main(String [] args) {
+        maxTree(new int[]{2, 5, 6, 0, 3, 1});
+    }
+}
