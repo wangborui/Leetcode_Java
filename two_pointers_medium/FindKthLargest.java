b42c359798cf0bd8b731fd4c283a9002ccf9eae1
Borui Wang <wangbd@bc.edu>
2017-01-31 11:02:34 -0600

    Updated Comments

--- two_pointers_medium/FindKthLargest.java
+++ two_pointers_medium/FindKthLargest.java
@@ -25,13 +25,40 @@ import java.util.Random;
  *
  * @author Borui Wang
  */
-//if k = 1, meaning find the largest number, which is the last number after sorting array
-//we can also look at k = length - k smallest number, if k = 1, length = 6, then length - 1 = 5, which is the last number in the sorted array
-//choose a random pivot number, partition the array with p, <= p to the left, > p to the right
-//after partition, if index of p = length - index = k, return p
-//if length - index > k, partition index 
+
 public class FindKthLargest {
-    //O(n log n) sorting algo
+    /**
+     * O(n log n) sorting algorithm
+     * @param nums
+     * @param k
+     * @return kth largest element
+     * Analysis:
+     * 
+     * This problem is asking us to find the kth largest element in the array if the array is sorted.
+     * this is the same as finding the element with index n - k, why?
+     * 
+     *      if k = 1, meaning find the largest number, which is the last number after sorting array
+     *      we can also look at k = length - k smallest number, if k = 1, length = 6, then length - 1 = 5, which is the last number in the sorted array
+     *      choose a random pivot number, partition the array with p, <= p to the left, > p to the right
+     *
+     * Because there are duplicates in the array, we choose a 3 way partition method with this invariant, based on the following methodology
+     * A number is less than pivot, 
+     * A number is equal to pivot,
+     * A number is greater than pivot
+     *      
+     *      During partition:
+     *      <,<,<,=,=,=,{..unknown..},>,>,>
+     *             i      j            k
+     *  
+     *      After partition:
+     *      <,<,<,=,=,=,=,=,=,>,>,>,>,>,>,
+     *             i           k j
+     * 
+     * Pitfalls:
+     * 
+     *      1.) pointer k will always point to the pivot element 
+     *      2.) we need to use random generator to choose pivot number randomly every time
+     */
     public int findKthLargestSort(int[] nums, int k) {
         Arrays.sort(nums);
         int N = nums.length;
@@ -39,50 +66,48 @@ public class FindKthLargest {
     }
 
     //randomize pivot with partition O(n)
-    private static Random rand = new Random();
 
+    //invariant <<<< | ====== | unknown | >>>>>>
+    //                 i       j       k
+    private static final Random rand = new Random();
     static int findKthLargest(int[] nums, int k) {
-        //this case is undefined!
-        if (nums == null || nums.length == 0) {
+        //this case will not happen
+        if(nums == null || nums.length == 0 || k < 1 || k > nums.length) {
             return -1;
         }
-        k = nums.length - k;
-        int res = partition(nums, k, 0, nums.length - 1);
-        return res;
+        int n = nums.length;
+        k = n - k;
+        return partition(nums, 0, n - 1, k);
     }
-
-    static int partition(int[] nums, int target, int start, int end) {
-
-        int position = start;
-        if (end - start > 0) {
-            position = start + rand.nextInt(end - start);
-        }
-        int pivot = nums[position];
-        int i = start;
-        int k = start;
-        int j = end;
-
-        //uses three way partition
-        while (i <= j) {
-            if (nums[i] == pivot) {
-                i++;
-            } else if (nums[i] < pivot) {
-                swap(nums, i++, k++);
+    static int partition(int[] nums, int start, int end, int target) {
+        int i = start; 
+        int j = start; 
+        int k = end;
+        int bound = start;
+        if(end - start != 0) {
+            bound = start + rand.nextInt(end - start);
+        }  
+        int pivot = nums[bound];
+        
+        while(j <= k) {
+            if(nums[j] < pivot) {
+                swap(nums, i++, j++);
+            } else if(nums[j] == pivot) {
+                j++;
             } else {
-                swap(nums, i, j--);
-            }
+                swap(nums, j, k--);
+            } 
         }
-
-        if (j == target) {
-            return nums[j];
-        } else if (j < target) {
-            return partition(nums, target, j + 1, end);
+        
+        if(k == target) {
+            return nums[k];
+        } else if(k < target) {
+            return partition(nums, k + 1, end, target);
         } else {
-            return partition(nums, target, start, j - 1);
+            return partition(nums, start, k - 1, target);
         }
     }
-
-    static void swap(int[] nums, int i, int j) {
+    static void swap(int[]nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
