2fe939ea9085eb754fd3e7524f21fe25b26fe95a
Borui Wang <wangbd@bc.edu>
2017-02-17 15:23:11 -0600

    Adding Space Optimization and Comments

--- dynamic_programming_easy/Rob.java
+++ dynamic_programming_easy/Rob.java
@@ -1,41 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package dynamic_programming_easy;
+//Source : https://leetcode.com/problems/house-robber/
+//Date   : 02/17/2017
+
+/*******************************************************************************
+ You are a professional robber planning to rob houses along a street. 
+ * Each house has a certain amount of money stashed, the only constraint stopping 
+ * you from robbing each of them is that adjacent houses have security system connected 
+ * and it will automatically contact the police if two adjacent houses were broken into 
+ * on the same night.
+ * 
+ * Given a list of non-negative integers representing the amount of money of each house, 
+* determine the maximum amount of money you can rob tonight without alerting the police.
+*******************************************************************************/
+package Leetcode_Java.dynamic_programming_easy;
 
 /**
  *
  * @author Borui Wang
  */
 public class Rob {
-    //first try
-//    static int rob(int[] nums) {
-//        int n = nums.length;
-//        int [] result = new int[n];
-//        return rob(nums,result,n-1);
-//    }
-//    static int rob(int [] nums, int[] result, int start){
-//        if(start < 0) return 0;
-//        result[start] = (int)Math.max(rob(nums, result, start-2)+nums[start], rob(nums,result,start-1));
-//        return result[start];
-//    }
-    //my working solutions
+    /*
+ * Dynamic Programming
+ * dp[n] means max values we can get from first i houses, not including the ith house
+ * We can easy find the recurive fomular:
+ *
+ *     dp[n] = max( 
+ *                    dp[n-1],   // the previous house has been robbed. 
+ *                    dp[n-2] + money[n]  // the previous house has NOT been robbed.
+ *                )
+ *                  
+ * The initalization is obvious:
+ *     dp[0] = 0
+ *     dp[1] = money[0]
+ *
+ */
+    //Time O(n), space O(n)
     static int rob(int[] nums) {
-       int n = nums.length;
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        if(n==2) return Math.max(nums[0], nums[1]);
-        int [] result = new int[n+1];
-        result[0] = 0;
-        result[1] = nums[0];
-        result[2] = Math.max(nums[0], nums[1]);
-        for(int i = 3; i <= n;i++){
-            result[i] = Math.max(result[i-2]+nums[i-1], result[i-1]);
+        if(nums == null || nums.length == 0) {
+            return 0;
         }
-        return result[n];
+        //dp[i] means max values we can get from first i houses, not including the ith house
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
         
+        //dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+        for(int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        
+        return dp[n];
+    }
+    //Space Optimized to O(1)
+    static int robOptimized(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        //f[i] means max values we can get from first i houses, not including the ith house
+        int n = nums.length;
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        
+        //f[i] = max(f[i - 1], f[i - 2] + nums[i - 1])
+        for(int i = 2; i <= n; i++) {
+            dp[i%2] = Math.max(dp[(i - 1)%2], dp[(i - 2)%2] + nums[i - 1]);
+        }
+        
+        return dp[n%2];
     }
     public static void main(String[] args){
         System.out.println(rob(new int[]{3,1,5,8,0,4,12}));
