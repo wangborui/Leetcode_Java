09fefa1b7352db051e0687a9fc513405a027d4c3
Borui Wang <wangbd@bc.edu>
2017-03-10 18:55:14 -0600

    added comments

--- linked_list_medium/ReverseList2.java
+++ linked_list_medium/ReverseList2.java
@@ -32,7 +32,14 @@ public class ReverseList2 {
             val = x;
         }
     }
-
+    //需要建立虚拟链表，因为我们不知道反转后链表开始节点是什么
+    //这里我们需要记录几个重要的节点preM, M，N，posN 如图
+    //需要先把N 节点向后移动 n - m个位置，然后再把M和N节点一起向后移动，知道M节点就位
+    //反转m 和 n之间的所有节点
+//    pos:    1    2    3    4    5    6    7
+//            1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7  m = 3, n = 5
+//                 |    |         |    |
+//                preM  M         N   posN
     static ListNode reverseBetween(ListNode head, int m, int n) {
         ListNode dummy = new ListNode(-1);
         dummy.next = head;
