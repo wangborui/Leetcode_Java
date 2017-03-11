ccdce470aab369737b315b1865435704aea970ca
Borui Wang <wangbd@bc.edu>
2017-03-10 18:44:21 -0600

    added comments

--- linked_list_medium/RemoveDuplicates.java
+++ linked_list_medium/RemoveDuplicates.java
@@ -32,31 +32,11 @@ public class RemoveDuplicates {
             val = x;
         }
     }
-
-    //uses two pointers like in remove duplicates from sorted arrays
-    //invariant head->unique elements -> i -> duplicates -> j -> unknown
-
-    public ListNode deleteDuplicates(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-
-         ListNode i = head;
-         ListNode j = head;
-
-        while (j != null) {
-            //record current value
-            int val = j.val;
-            //j stops on the first different element
-            while (j != null && j.val == val) {
-                j = j.next;
-            }
-            i.next = j;
-            i = i.next;
-        }
-        return head;
-    }
+ 
     //this solution only keeps one pointer, if that is equal to its next, 
+    //我们不需要使用虚拟节点，因为在取掉重复节点后，我们的开头节点任然是原链表的开始节点（题意）
+    //使用walker节点来遍历整个链表，如果walker的后面节点和walker的值是一样的，那么我们把walker的下个节点指向下下个节点
+    //如果walker节点的下个节点值和walker值不同，walker则向后移动一个节点
     public ListNode deleteDuplicatesOnePointer(ListNode head) {
         if (head == null) {
             return head;
