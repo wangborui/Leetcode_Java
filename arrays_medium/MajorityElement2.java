d65cc03f8fd973c3c536b196290ab651b6c1c968
Borui Wang <wangbd@bc.edu>
2017-03-17 10:14:51 -0500

    added comments

--- arrays_medium/MajorityElement2.java
+++ arrays_medium/MajorityElement2.java
@@ -20,13 +20,19 @@ import java.util.List;
  * @author Borui Wang
  */
 public class MajorityElement2 {
-
+//    这个题要求我们要O（1）的空间，如此的话就只能设置变量而不能用哈希表了
+//    因为最多出现两个majority element 都超过总数的1/3，设置number1 和number2，count1和count2来记录这两个数字出现的次数
+//    遍历整个数组，看看当前的数字是不是number1和number2里面其中的一个，如果是的话增加他们出现的次数
+//    如果不是的话，看看number1和number2 是否都还未被设置，设置其中一个为当前数字
+//    再不然的话就把count1和count2都减1
+    
+   
     static List<Integer> majorityElement(int[] nums) {
         List<Integer> result = new ArrayList();
         if (nums == null || nums.length == 0) {
             return result;
         }
-        int number1 = nums[0], number2 = nums[0], count1 = 0, count2 = 0, len = nums.length;
+        int number1 = 0, number2 = 0, count1 = 0, count2 = 0, len = nums.length;
        
 //         Inside for loop
 //         check num == number1 or num == number2 before checking count for
