23ea999b188a4c00fb162762cf753a3101bea9e7
Borui Wang <wangbd@bc.edu>
2017-03-18 17:22:00 -0500

    added comments

--- math_easy/Sqrt.java
+++ math_easy/Sqrt.java
@@ -14,8 +14,8 @@ package Leetcode_Java.math_easy;
  *
  * @author Borui Wang
  */
-可以用二分查找来解决，二分查找找到一个整数的平方是刚刚好等于x，或者找到最大的一个数他的平方刚刚小于x
-为了防止溢出，我们先把low 设置为0，类型设置为long；high设置为x，类型设置为long
+//可以用二分查找来解决，二分查找找到一个整数的平方是刚刚好等于x，或者找到最大的一个数的平方刚刚小于x
+//为了防止溢出，我们先把low 设置为0，类型设置为long；high设置为x，类型设置为long
 public class Sqrt {
     //use binary search assume low = 0, high = x
         public int mySqrt(int x) {
