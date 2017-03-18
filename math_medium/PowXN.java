54828d3ed33952b9427346d2a2d1d09e2b6e0ffc
Borui Wang <wangbd@bc.edu>
2017-03-18 17:10:02 -0500

    added comments

--- math_medium/PowXN.java
+++ math_medium/PowXN.java
@@ -24,15 +24,15 @@ public class PowXN {
  *   Time O(n)
  *   However, 
  *
- *     1) We need think about the `n` is negtive number.
+ *     1) We need to think about the `n` is negtive number.
  *
- *     2) We need more wisely deal with the following cases:
+ *     2) We need to more wisely deal with the following cases:
  *
  *         pow(1, MAX_INT);
  *         pow(-1,BIG_INT);
  *         pow(2, BIG_INT);
  *
- *        To deal with such kind case, we can use x = x*x to reduce the `n` more quickly
+ *        To deal with such kind of case, we can use x = x*x to reduce the `n` more quickly
  *
  *        so, 
  *             if `n` is an negative, we can `x = 1/x`, and `n = -n;`
@@ -41,6 +41,13 @@ public class PowXN {
         Edge Case: x can get really big, so we need to check if x is inifinite then return 0
  *   Time O(log n)
  */
+//    有三种情况需要考虑
+//            n是一个负数，我们把x设置为 1/x, n变成-n
+//            n是一个偶数，我们可以递归的计算 myPow（x * x，n /2)
+//            n是一个奇数，我们可以递归的计算 x * myPow（x * x，n/2)
+//    需要注意的有两个：
+//            当n = 0的时候，我们直接返回1
+//            当x变得无限大的时候，我们需要返回0
     public double myPow(double x, int n) {
         if(n == 0)
             return 1;
