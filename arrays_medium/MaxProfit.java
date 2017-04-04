69978f3a1968af6c8e8b3f9bafca2e6f3b810e62
Borui Wang <wangbd@bc.edu>
2017-04-03 20:54:47 -0500

    max profit

--- arrays_medium/MaxProfit.java
+++ arrays_medium/MaxProfit.java
@@ -23,6 +23,9 @@ public class MaxProfit {
 //find the largest price difference between peak price and trough price, make sure trough happens before peak price
 //traverse all price points, for each price i, assume we sell at price i, and buy at min price from prices[0:i - 1]
 //update global profit if i - min(prices[0 : i - 1])
+//    建立两个变量股票最低交易额和最大利润额
+//    遍历整个价格数组，如果当前价格小于最低交易额，就把当前价格设置成最低交易额
+//    用当前价格减去最低交易额，更新最大利润额
     public int maxProfit(int[] prices) {
         //if we do not buy at all we get 0 dollars as profit
         int max = 0;
