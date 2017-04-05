973295859147583607f9d5a2425feb0c1b2ca388
Borui Wang <wangbd@bc.edu>
2017-04-05 15:50:19 -0500

    largest BST subtree

--- tree_medium/LargestBSTSubtree.java
+++ tree_medium/LargestBSTSubtree.java
@@ -1,7 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+// Source : https://leetcode.com/problems/largest-bst-subtree/#/description
+// Date   : 04/05/2017
+/**
+ * ********************************************************************************
+ * Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
+ *
+ * Note:
+ * A subtree must include all of its descendants.
+ * Here's an example:
+ * 10
+ * / \
+ * 5  15
+ * / \   \
+ * 1   8   7
+ * The Largest BST Subtree in this case is the highlighted one.
+ * The return value is the subtree's size, which is 3.
+ * Hint:
+ *
+ * You can recursively use algorithm similar to 98. Validate Binary Search Tree at each node of the tree, which will result in O(nlogn) time complexity.
+ * Follow up:
+ * Can you figure out ways to solve it with O(n) time complexity?
+ *********************************************************************************
  */
 package Leetcode_Java.tree_medium;
 
@@ -12,7 +30,6 @@ package Leetcode_Java.tree_medium;
 //从下至上的搜索二叉查找树，耗费时间O(n)
 //先分治的查找左子树和右子树的最大二叉查找树，
 //如果左右两边都分别是二叉查找树，那么由当前根节点组成的必然也是二叉查找树，跟新全局最大值
-
 public class LargestBSTSubtree {
 
     public class TreeNode {
@@ -40,18 +57,20 @@ public class LargestBSTSubtree {
     }
     protected int max = 0;
 //    使用全局变量
+
     public int largestBSTSubtree(TreeNode root) {
         find(root);
         return max;
     }
+
     private Result find(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE);
         }
         Result left = find(root.left);
         Result right = find(root.right);
-        
-        if(left.size < 0 || right.size < 0 || left.upper >= root.val || right.lower <= root.val) {
+
+        if (left.size < 0 || right.size < 0 || left.upper >= root.val || right.lower <= root.val) {
             return new Result(-1, Integer.MAX_VALUE, Integer.MIN_VALUE);
         }
         int s = left.size + 1 + right.size;
@@ -59,6 +78,7 @@ public class LargestBSTSubtree {
         return new Result(s, Math.min(left.lower, root.val), Math.max(right.upper, root.val));
     }
 //    不使用全局变量，所有的都是当地变量
+
     public int largestBSTSubtree2(TreeNode root) {
         Result res = find(root);
         return Math.abs(res.size);
@@ -77,5 +97,5 @@ public class LargestBSTSubtree {
         int s = left.size + 1 + right.size;
         return new Result(s, Math.min(left.lower, root.val), Math.max(right.upper, root.val));
     }
-    
+
 }
