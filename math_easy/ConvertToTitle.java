cd29149eca52486f1ae7cb4856bb3d6fa28c8726
Borui Wang <wangbd@bc.edu>
2017-04-04 19:21:37 -0500

    Convert To Title

--- math_easy/ConvertToTitle.java
+++ math_easy/ConvertToTitle.java
@@ -1,9 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package math_easy;
+// Source : https://oj.leetcode.com/problems/excel-sheet-column-title/
+// Date   : 04/04/2017
+
+/********************************************************************************** 
+ * 
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ * 
+ * For example:
+ * 
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB 
+ * 
+ * Credits:Special thanks to @ifanchu for adding this problem and creating all test cases.
+ *               
+ **********************************************************************************/
+package Leetcode_Java.math_easy;
 
 /**
  *
@@ -11,12 +26,14 @@ package math_easy;
  */
 public class ConvertToTitle {
     static String convertToTitle(int n) {
-        StringBuilder sb=  new StringBuilder();
-        while(n>0){
-            char temp = (char)('A'+(n-1)%26);
-            sb.append(""+temp);
+        StringBuilder sb = new StringBuilder();
+        while(n > 0) {
+            n--;
+            char c = (char) ('A' + n % 26);
+            sb.insert(0, "" + c);
+            n /= 26;
         }
-        return sb.reverse().toString();
+        return sb.toString();
     }
     public static void main(String[] args){
         //convertToTitle(1);
