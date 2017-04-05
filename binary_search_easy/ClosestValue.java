7933ba5dae7ade4ef5fca9eaefa72ce0542267c7
Borui Wang <wangbd@bc.edu>
2017-04-05 17:29:21 -0500

    closest value

--- binary_search_easy/ClosestValue.java
+++ binary_search_easy/ClosestValue.java
@@ -1,59 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+//Source: https://leetcode.com/problems/closest-binary-search-tree-value/#/description
+//Date  : 04/05/2017
+/**
+ * ********************************************************************************
+ * Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+ *
+ * Note:
+ * Given target value is a floating point.
+ * You are guaranteed to have only one unique value in the BST that is closest to the target.
+ *********************************************************************************
  */
-package binary_search_easy;
+package Leetcode_Java.binary_search_easy;
 
 /**
  *
  * @author Borui Wang
  */
 public class ClosestValue {
-      public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode(int x) { val = x; }
-  }
-      /*
-      recursive solution
-      public int closestValue(TreeNode root, double target) {
-    int a = root.val;
-    TreeNode kid = target < a ? root.left : root.right;
-    if (kid == null) return a;
-    int b = closestValue(kid, target);
-    return Math.abs(a - target) < Math.abs(b - target) ? a : b;
-}
-      */
-      /*
-      iterative
-      public int closestValue(TreeNode root, double target) {
-    int ret = root.val;   
-    while(root != null){
-        if(Math.abs(target - root.val) < Math.abs(target - ret)){
-            ret = root.val;
-        }      
-        root = root.val > target? root.left: root.right;
-    }     
-    return ret;
-}
-      */
-      //my solution
-//    public int closestValue(TreeNode root, double target) {
-//        return closestValue(root, target, root.val);
-//    }
-//    public int closestValue(TreeNode root, double target, int close){
-//        if(root == null) return close;
-//        else{
-//            if(root.val == target) return root.val;
-//            else {
-//                TreeNode kid= root.val > target?root.left:root.right;
-//                if(Math.abs(root.val - target) < Math.abs(close - target))
-//                    close = root.val;
-//                return closestValue(kid, target, close);
-//            
-//            }
-//        }
-//    }
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+//    通过二分的方式查找和target值最接近的节点值
+//    如果当前值大于target值，那么我们要递归的查找其左子树
+//    如果当前值小于target值，那么我们要递归的查找其右子树
+    public int closestValue(TreeNode root, double target) {
+        int val = root.val;
+        TreeNode child = target < val ? root.left : root.right;
+        if (child == null) {
+            return val;
+        } else {
+            int sub = closestValue(child, target);
+            return Math.abs(val - target) < Math.abs(sub - target) ? val : sub;
+        }
+    }
 }
--- bit_manipulation_medium/SingleNumber2.java
+++ bit_manipulation_medium/SingleNumber2.java
@@ -1,16 +1,16 @@
-/*
-Source: https://leetcode.com/problems/single-number-ii/
-Date  : 01/09/2017
-/********************************************************************************** 
-* 
-* Given an array of integers, every element appears three times except for one. Find that single one.
-* 
-* Note:
-* Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
-* 
-*               
-**********************************************************************************/
-
+//Source: https://leetcode.com/problems/single-number-ii/
+//Date  : 01/09/2017
+/**
+ * ********************************************************************************
+ *
+ * Given an array of integers, every element appears three times except for one. Find that single one.
+ *
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ *
+ *********************************************************************************
+ */
 package Leetcode_Java.bit_manipulation_medium;
 
 /**
@@ -18,35 +18,39 @@ package Leetcode_Java.bit_manipulation_medium;
  * @author Borui Wang
  */
 public class SingleNumber2 {
+
     /**
-     * 
+     *
      * @param nums[]
-     * @return res
-     * we know that integers in Java has 32 bits, and we create an integer array to hold all 32 bits, they are all zero initially.
-     * 
+     * @return res we know that integers in Java has 32 bits, and we create an
+     * integer array to hold all 32 bits, they are all zero initially.
+     *
      * we also know that all numbers appears 3 times except for one
-     * 
-     * for ith-bit in bits[], we count how many times 1 appears in that bit for each number in nums
-     * 
-     * after counting ith-bit of all numbers in nums array, we mod that bit  value by 3, if result is 0, then this bit is not used in our answer.
-     * 
+     *
+     * for ith-bit in bits[], we count how many times 1 appears in that bit for
+     * each number in nums
+     *
+     * after counting ith-bit of all numbers in nums array, we mod that bit
+     * value by 3, if result is 0, then this bit is not used in our answer.
+     *
      * if result is 1, we use this in our final answer
-     * 
-     * This solution can be easy to extend to "every element appears k times except for one."
+     *
+     * This solution can be easy to extend to "every element appears k times
+     * except for one."
      */
 //    integer 在 java 里面有三十二位， 把每个数的[0:31]位都亦或起来
 //    当第i位被亦或完毕后，看这一位在原来的数组中出现了多少次 然后再 % 3，可能的结果有 0（出现三次的数第i位）， 或者1（出现1次数的第i位）
 //    再把这一位加到我们之前的结果当中 res = res | bit （往左移i次）
     public int singleNumber(int[] nums) {
-        if(nums == null || nums.length == 0) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
-        
+
         int res = 0;
-        int []bits = new int[32];
-        
-        for(int i = 0; i < 32; i++) {
-            for(int j = 0; j< nums.length; j++) {
+        int[] bits = new int[32];
+
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < nums.length; j++) {
                 bits[i] += ((nums[j] >> i) & 1);
             }
             bits[i] %= 3;
@@ -54,8 +58,8 @@ public class SingleNumber2 {
         }
         return res;
     }
-    
-        /*
+
+    /*
      *   The following solution is popular solution on Internet, but it looks it's not easy to understand.
      *
      *   Actually, it just optimizes the above soultion.
@@ -72,11 +76,11 @@ public class SingleNumber2 {
      *
      */
     static int singleNumberOptimized(int[] nums) {
-          int ones = 0, twos = 0, threes = 0;
-          int n = nums.length;
+        int ones = 0, twos = 0, threes = 0;
+        int n = nums.length;
         for (int i = 0; i < n; i++) {
             // `ones & A[i]` the result is the bitmask which the bits appeared twice
-            twos |= ones & nums[i]; 
+            twos |= ones & nums[i];
             // XOR means remove the bit which appeared twice int `ones` 
             ones ^= nums[i];
             // count the `three`
@@ -87,7 +91,8 @@ public class SingleNumber2 {
         }
         return ones;
     }
+
     public static void main(String[] args) {
-        System.out.println(singleNumberOptimized(new int[]{12,2,12,9,2,12,2}));
+        System.out.println(singleNumberOptimized(new int[]{12, 2, 12, 9, 2, 12, 2}));
     }
 }
