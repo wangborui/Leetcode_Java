c177e893f7b39e689db32f3dc7cc202c0d4f4c62
Borui Wang <wangbd@bc.edu>
2017-05-24 12:19:48 -0500

    valid word square

--- trie_hard/ValidWordSquare.java
+++ trie_hard/ValidWordSquare.java
@@ -1,7 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+//Source : https://leetcode.com/problems/valid-word-square/#/description
+//Date   : 05/24/2017
+/**
+ * ******************************************************************************
+ * Given an integer array, find a subarray with sum closest to zero.
+ * Return the indexes of the first number and last number.
+ *
+ * Have you met this question in a real interview? Yes
+ * Example
+ * Given [-3, 1, 1, -3, 5], return [0, 2], [1, 3], [1, 1], [2, 2] or [0, 4].
+ *
+ * Challenge
+ * O(nlogn) time
+ *******************************************************************************
  */
 package Leetcode_Java.trie_hard;
 
