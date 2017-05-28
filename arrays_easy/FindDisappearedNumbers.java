c0d93d143c8d0e825836c4f550a313ad582a383b
Borui Wang <wangbd@bc.edu>
2017-05-28 13:15:36 -0500

    heaters

--- arrays_easy/FindDisappearedNumbers.java
+++ arrays_easy/FindDisappearedNumbers.java
@@ -1,7 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+// Source : https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/#/description
+// Date   : 05/27/2017
+/**
+ * ********************************************************************************
+ * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+ *
+ * Find all the elements of [1, n] inclusive that do not appear in this array.
+ *
+ * Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ *
+ * Example:
+ *
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * Output:
+ * [5,6]
+ *********************************************************************************
  */
 package Leetcode_Java.arrays_easy;
 
