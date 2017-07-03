01782081747d651bbae6207324a9592031d41506
Borui Wang <wangbd@bc.edu>
2017-07-02 21:22:15 -0500

    adding a more concise method

--- dynamic_programming_medium/UniquePathWithObstacles2.java
+++ dynamic_programming_medium/UniquePathWithObstacles2.java
@@ -69,7 +69,41 @@ public class UniquePathWithObstacles2 {
 
     }
 
+    static int uniquePathsWithObstacles2(int[][] grid) {
+        int m = grid.length;
+        if (m == 0) {
+            return 0;
+        }
+
+        int n = grid[0].length;
+        if (n == 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = 0;
+                    if (i > 0) {
+                        dp[i][j] += grid[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += grid[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
-        System.out.println(uniquePathsWithObstacles(new int[][]{{1}, {0}}));
+        //System.out.println(uniquePathsWithObstacles(new int[][]{{1}, {0}}));
+        System.out.println(uniquePathsWithObstacles2(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
     }
 }
