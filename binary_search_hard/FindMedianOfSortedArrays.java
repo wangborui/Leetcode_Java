cbaf7f41058b55968a147767fab65a58213c3def
Borui Wang <wangbd@bc.edu>
2017-08-13 20:42:12 -0500

    Median of two sorted arrays

--- binary_search_hard/FindMedianOfSortedArrays.java
+++ binary_search_hard/FindMedianOfSortedArrays.java
@@ -26,17 +26,17 @@ public class FindMedianOfSortedArrays {
         //if array size is odd [1,2,3] = 3, return A[mid] 3 / 2 = 1, A[1] = 2
         //if array size is even [1,2,3,4] = 4, return (A[mid] + A[mid - 1]) / 2.0
         //  mid = 4 / 2 = 2, mid - 1 = 1, return (2 + 3) / 2.0 = 2.5
-        if(nums1 == null || nums1.length == 0) {
-            return nums2.length % 2 == 1 
-                 ?  nums2[nums2.length / 2] 
-                 : (nums2[nums2.length / 2] + nums2[nums2.length / 2 - 1]) / 2.0;
-        }
-        
-        if(nums2 == null || nums2.length == 0) {
-            return nums1.length % 2 == 1 
-                 ?  nums1[nums1.length / 2] 
-                 : (nums1[nums1.length / 2] + nums1[nums1.length / 2 - 1]) / 2.0;
-        }
+//        if(nums1 == null || nums1.length == 0) {
+//            return nums2.length % 2 == 1 
+//                 ?  nums2[nums2.length / 2] 
+//                 : (nums2[nums2.length / 2] + nums2[nums2.length / 2 - 1]) / 2.0;
+//        }
+//        
+//        if(nums2 == null || nums2.length == 0) {
+//            return nums1.length % 2 == 1 
+//                 ?  nums1[nums1.length / 2] 
+//                 : (nums1[nums1.length / 2] + nums1[nums1.length / 2 - 1]) / 2.0;
+//        }
         
         int m = nums1.length;
         int n = nums2.length;
@@ -87,10 +87,49 @@ public class FindMedianOfSortedArrays {
             return findKth(nums1, oneStart, nums2, twoStart + k / 2, k - k / 2);
         }
     }
- 
+     static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int total = m + n;
+        
+        if(total % 2 == 1) {
+            return findKth2(nums1, 0, m, nums2, 0, n, total / 2 + 1);
+        } else {
+            return (findKth2(nums1, 0, m, nums2, 0, n, total / 2 + 1)
+                   +findKth2(nums1, 0, m, nums2, 0, n, total / 2)) / 2;
+        }
+    }
+    static double findKth2(int[] a, int sa, int m, int[] b, int sb, int n, int k) {
+        if(m > n) {
+            return findKth2(b, sb, n, a, sa, m, k);
+        }
+        
+        if(m == 0) {
+            return b[sb + k - 1];
+        }
+        
+        if(k == 1) {
+            return Math.min(a[sa], b[sb]);
+        }
+        
+        int ka = Math.min(k / 2, m);
+        int kb = k - ka;
+        
+        if(a[sa + ka - 1] == b[sb + kb - 1]) {
+            return a[sa + ka - 1];
+        } else if(a[sa + ka - 1] < b[sb + kb - 1]) {
+            //get rid of left half of a
+            return findKth2(a, sa + ka, m - ka, b, sb, n, k - ka);
+        } else {
+            return findKth2(a, sa, m, b, sb + kb, n - kb, k - kb);
+        }
+    }
     public static void main(String[] args) {
-        int[]a ={1,3};
-        int[]b={2};
+        int[]a ={};
+        int[]b={3};
+//        int[]a ={};
+//        int[]b={};
         System.out.println(findMedianSortedArrays(a,b));
+        //System.out.println(findMedianSortedArrays2(a,b));
     }
 }
